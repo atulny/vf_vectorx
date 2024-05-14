8e7dbec8a1db7177c5b0149d6639c336503420af
Atul Sharma <atulny@gmail.com>
2024-05-14 01:41:37 -0400

    updates

--- pages/2_Settings.py
+++ pages/2_Settings.py
@@ -13,14 +13,23 @@ def render():
     col1,col2 = st.columns([1,4],)
     with col1:
         st.markdown("<div class='full-length shaded'></div>", unsafe_allow_html=True)
-        selected = option_menu("Settings", ["My Profile","Privacy Message","Logout"],
+        selected = option_menu("Settings", ["My Profile","Privacy Message"],
                                menu_icon="gear",icons=["credit-card-2-front"], default_index=0)
         if selected:
             st.session_state.selected_setting = selected
 
     with col2:
-        st.title(st.session_state.selected_setting )
-
+            st.subheader("Privacy Message")
+            if selected == "Privacy Message":
+                st.markdown("""
+                Your privacy is our priority. You can be sure that your information and conversations stay between you 
+                and your virtual friends.
+                
+                You get to choose what you share, how you show up online, or who you talk to.
+                
+                We add additional layers of protection to all of your conversations
+                 and ensure that the data is stored in our secure databases.
+                """)
 
 
 

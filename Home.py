48bdc4df3cdf67ca32eecc30aef25999b40fc2a0
Atul Sharma <atulny@gmail.com>
2024-05-14 00:01:43 -0400

    updates

--- Home.py
+++ Home.py
@@ -56,7 +56,7 @@ with st.container():
                 try:
                     if current_status == "active" or current_status == "No session":
                         ## HACK ALERT
-                        streamlit_js_eval(js_expressions="top.location.reload()")
+                        streamlit_js_eval(js_expressions="window.location.reload()")
                 except:
                     pass
             #print(st.session_state.usersession)
@@ -65,7 +65,7 @@ with st.container():
             st.subheader("MISSION")
             st.markdown("""Chat app for people to communicate with a virtual friend when they are feeling lonely or just need someone to talk to. Virtual friend demographics can be random or personalized. Y"
                             our virtual friend gets to know you and remembers to check-in on occasion. Powered by LLM.""")
-            streamlit_js_eval(js_expressions="self != top && top.reload()")
+            streamlit_js_eval(js_expressions="self.parent != top && top.reload()")
             set_bg_hack("./asset/hero-1-5.png")
             streamlit_js_eval(js_expressions=f"""
                 let fn = ()=>{{         

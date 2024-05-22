06eb8183a8165bccb54e39b73cbb2cfd2b4bfe00
Atul <atulny@gmail.com>
2024-05-22 16:34:47 -0400

    Update 2_Settings.py

--- pages/2_Settings.py
+++ pages/2_Settings.py
@@ -10,18 +10,7 @@ from form_util import render_fields
 from util import page_init, auth_decorator
 
 st.set_page_config(layout="wide",page_title = "Settings")
-
-# class Location(BaseModel):
-#     city: Optional[str] = None
-#     state: Optional[str] = None
-#     country: Optional[str] = "USA"
-# class UserProfile(BaseModel):
-#     #user_id: Optional[str] = None
-#     first_name: Optional[str] = None
-#     last_name: Optional[str] = None
-#     dob_ts: Optional[int] = None
-#     gender: Optional[str] = None
-#     location : Optional[Location]=None
+ 
 
 User_Profile=[
     {"name":"first_name","type": str},
